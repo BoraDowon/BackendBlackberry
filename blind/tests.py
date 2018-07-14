7ff0380a54488c6232bc3f322bedc7531f19df13
Dowon <blessjdw@gmail.com>
2018-07-14 20:49:21 +0900

    added sample test code

--- blind/tests.py
+++ blind/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class BlackberryTest(TestCase):
+
+    def test_example(self):
+        arr = [1, 2, 3, 4]
+        self.assertIs(len(arr), 4)
