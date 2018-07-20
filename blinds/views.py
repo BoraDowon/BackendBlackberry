708f5e9cf3e7fa72286ace7bac24646fa4a44ec4
akahard2dj <akahard2dj@naver.com>
2018-07-20 10:59:51 +0900

    Create views.py

---
+++ blinds/views.py
@@ -0,0 +1,15 @@
+from rest_framework.views import APIView
+
+
+class BlindDetail(APIView):
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
