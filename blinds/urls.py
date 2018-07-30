f1c2b901edc03a62589a7f219bd0afb1ddd33d9f
akahard2dj <akahard2dj@naver.com>
2018-07-30 17:19:48 +0900

    Update urls.py

--- blinds/urls.py
+++ blinds/urls.py
@@ -4,6 +4,5 @@ from blinds import views
 
 urlpatterns = [
     path('blinds/<int:board_id>/', views.BlindList.as_view()),
-    path('blinds/<int:board_id>/', views.BlindCreateArticle.as_view()),
     path('blinds/<int:board_id>/articles/<int:article_id>/', views.BlindDetail.as_view()),
 ]
