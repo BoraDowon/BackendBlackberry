d205aade2a63d2b14bcba7cd8ea0c7f6dc07430c
akahard2dj <akahard2dj@naver.com>
2018-07-30 15:22:56 +0900

    Update urls.py

--- blinds/urls.py
+++ blinds/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from blinds import views
 
 urlpatterns = [
-    path('blind/', views.BlindDetail.as_view())
+    path('blinds/<int:board_id>/', views.BlindList.as_view()),
+    path('blinds/<int:board_id>/', views.BlindCreateArticle.as_view()),
+    path('blinds/<int:board_id>/articles/<int:article_id>/', views.BlindDetail.as_view()),
 ]
