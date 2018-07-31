52ecadaf5d5af750b34173a627a107148233c9c2
akahard2dj <akahard2dj@naver.com>
2018-07-31 11:13:10 +0900

    Update models.py

--- blinds/models.py
+++ blinds/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    board_id = models.IntegerField()
+    title = models.CharField(max_length=1000, blank=False)
+    body = models.CharField(max_length=2000, blank=False)
+    views = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
