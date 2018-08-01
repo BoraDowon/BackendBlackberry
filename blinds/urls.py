46b432bf22f282de48d3e9d665d61b42354a6524
akahard2dj <akahard2dj@naver.com>
2018-08-01 13:06:31 +0900

    Update urls.py

--- blinds/urls.py
+++ blinds/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from blinds import views
 
 urlpatterns = [
-    path('blinds/<int:board_id>/articles/',views.BlindList.as_view()),
-    path('blinds/<int:board_id>/articles/<int:article_id>/',views.BlindDetail.as_view()),
-    path('blinds/<int:baord_id>/articles/<int:article_id>/comments/',views.BlindCommentList.as_view()),
+    path('blinds/<int:board_id>/articles',views.BlindList.as_view()),
+    path('blinds/<int:board_id>/articles/<int:article_id>',views.BlindDetail.as_view()),
+    path('blinds/<int:baord_id>/articles/<int:article_id>/comments',views.BlindCommentList.as_view()),
 
 ]
