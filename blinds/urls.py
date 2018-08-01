74d8b066544da79eeb0134df4e28beb63c3916a0
akahard2dj <akahard2dj@naver.com>
2018-08-01 13:32:16 +0900

    url pattern 추가 작업

--- blinds/urls.py
+++ blinds/urls.py
@@ -5,6 +5,8 @@ from blinds import views
 urlpatterns = [
     path('blinds/<int:board_id>/articles',views.BlindList.as_view()),
     path('blinds/<int:board_id>/articles/<int:article_id>',views.BlindDetail.as_view()),
-    path('blinds/<int:baord_id>/articles/<int:article_id>/comments',views.BlindCommentList.as_view()),
-
+    path('blinds/<int:board_id>/articles/<int:article_id>/like', views.BlindArticleLike.as_view()),
+    path('blinds/<int:board_id>/articles/<int:article_id>/report', views.BlindArticleReport.as_view()),
+    path('blinds/<int:board_id>/articles/<int:article_id>/bookmark', views.BlindArticleBookmark.as_view()),
+    path('blinds/<int:board_id>/articles/<int:article_id>/comments',views.BlindCommentList.as_view()),
 ]
